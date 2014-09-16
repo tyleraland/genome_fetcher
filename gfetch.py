8a6f52eab292c029f963254eea5cc3e8da360b16
Tyler A Land <tland9@uw.edu>
2014-09-16 11:13:28 -0700

    Help messages.  Downloads all results now

--- gfetch.py
+++ gfetch.py
@@ -8,16 +8,25 @@ import os
 
 url = 'ftp.ncbi.nih.gov'
 
+# Note: NCBI has discontinued strain-level typing using TaxIDs.
+# For more info, see http://www.ncbi.nlm.nih.gov/news/11-21-2013-strain-id-changes/
+
 def get_args():
     parser = argparse.ArgumentParser(description=__doc__,
                                      formatter_class = argparse.RawDescriptionHelpFormatter)
     parser.add_argument('action', help="'search' or 'fetch'")
     parser.add_argument('-n', '--name', nargs=2,
-                        help='Scientific name, separated by spaces, not case sensitive')
+                        help="Scientific name, separated by spaces, case insensitive.\
+                              Name must exist somewhere in organism's listed name")
     parser.add_argument('-s', '--status', default="complete gapless_chromosome chromosome",
-                        help="Options: complete, gapless_chromosome, chromosome, chromosome_with_gaps, scaffold, contig")
+                        help="Types of genomes to consider, separated by spaces, case insensitive.\
+                              Options: complete gapless_chromosome chromosome\
+                                       chromosome_with_gaps scaffold contig\
+                              Default: complete gapless_chromosome chromosome")
     parser.add_argument('-o', '--outdir', default='genomes',
                         help="Directory to downlaod genomes to")
+    parser.add_argument('-v', '--verbose', action='store_true', default=False,
+                        help="Enable verbose search output")
 
     return parser.parse_args()
 
@@ -26,9 +35,9 @@ def main():
     if args.action not in ['search', 'fetch']:
         sys.stdout.write("Improper action '{}'.  See help".format(args.action))
 
+    # Metadata file which we will use to filter results and ultimately download data
     manifest = '/genomes/GENOME_REPORTS/prokaryotes.txt'
 
-    #ftp.login()                     # Login is anonymous, otherwise 'sftp' package is preferred
     host = ftputil.FTPHost(url, 'anonymous')
     host.download(manifest, 'genomes.txt')
     manifest_file = open('genomes.txt','r')
@@ -36,10 +45,12 @@ def main():
     meta = pandas.read_csv(manifest_file, header=0, sep='\t')
 
     if args.name:
+        # If a name was given, only look at organisms containing that name
         name = ' '.join(args.name)
         name = name[0].upper() + name[1:].lower()
         meta = meta[meta['Name'].str.contains(name, case=False)]
     if args.status: # [complete, gapless_chromosome, ...]
+        # If list of statuses was given, only look at organisms containing one of those
         statuses = args.status.split(' ')
         statuses = ['(' + status.replace('_', ' ') + ')' for status in statuses]
         statuses = '|'.join(statuses)
@@ -49,30 +60,26 @@ def main():
     meta = meta.drop_duplicates('TaxID') # Occasionally this signals duplicate strains
 
     if args.action == 'search':
+        if args.verbose:
+            print(meta)
+        else:
+            print('|' +
+                  '|\t|'.join(['Name', 'Group', 'Subgroup', 'Status']) +
+                  '|')
+            for i,row in meta.iterrows():
+                print('|' + 
+                      '|\t|'.join([row['Name'], row['Group'], row['SubGroup'], row['Status']]) +
+                      '|')
+    elif args.action == 'fetch':
+        try:
+            os.makedirs(args.outdir)
+        except OSError:
+            pass
+        basedir = 'genomes/ASSEMBLY_BACTERIA'
         for i,row in meta.iterrows():
-            print(row['Name'])
-
-    try:
-        os.makedirs(args.outdir)
-    except OSError:
-        pass
-    basedir = 'genomes/ASSEMBLY_BACTERIA'
-    for i,row in meta.iterrows():
-        chromosome = row['Chromosomes/RefSeq'][:-2] + '.fna'
-        uri = os.path.join(basedir,  row['FTP Path'],  chromosome)
-        #ftp.retrbinary('RETR {}'.format(uri), open(os.path.join(args.outdir, chromosome),'w').write)
-        host.download(uri, os.path.join(args.outdir, chromosome))
-        break
-#        print(row['FTP Path'])
-
-#    dirnames = ftp.nlst()          # Returns directory contents
-    
-#    for dirname in dirnames:
-#        if target.lower().replace(' ','_') in dirname.lower():
-#            ftp.cwd(dirname)
-#        if 'Staphylococcus
-#        
-#    print("staph aureus: {}".format(len([dirname for dirname in filenames if 'staphylococcus_aureus' in dirname.lower() ])))
+            chromosome = row['Chromosomes/RefSeq'][:-2] + '.fna'
+            uri = os.path.join(basedir,  row['FTP Path'],  chromosome)
+            host.download(uri, os.path.join(args.outdir, chromosome))
 
     host.close()
 
